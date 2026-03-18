e2e17805676923dd5150b6d4c6d57af4cc1209e6
Sameerasamiyath <sairasameera@gmail.com>
2026-03-18 16:42:18 +0400

    fix secret key for render

--- accounts/settings.py
+++ accounts/settings.py
@@ -15,7 +15,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-change-this-key'
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = True
 
